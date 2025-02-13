8226acd38cd78d3d26298d3b6f076d12d0ebee1c
Jacopo <jacopocarrozzo19@gmail.com>
2025-02-14 00:04:23 +0100

    Fix Facebook login issue

--- src/App.tsx
+++ src/App.tsx
@@ -68,15 +68,36 @@ function App() {
     }
   }
 
+  const getSkillLevel = (score: number) => {
+    if (score === 10) return "Maestro/a";
+    if (score >= 8) return "Esperto/a";
+    if (score >= 4) return "Intermedio";
+    return "Base";
+  };
+  
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-black font-extrabold text-4xl text-center mt-10">TRAVEL QUIZ</h1>
 
       {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-      <button className="cursor-pointer border-2 border-[#000000] shadow-md shadow-black/25 rounded-lg h-10 my-5 px-10 max-w-[200px]" onClick={startQuiz}>
+      <button className="cursor-pointer border-2 border-[#000000] bg-gray-200 shadow-md shadow-black/25 rounded-lg h-10 my-5 px-10 max-w-[200px]" onClick={startQuiz}>
         Start
       </button>) : null}
-      {userAnswers.length === TOTAL_QUESTIONS ? <p className="text-black text-2xl m-0">Score: {score}</p> : null}
+      {gameOver && userAnswers.length === 0 ? (
+        <div className="text-center p-5 bg-gray-200 rounded-lg shadow-md">
+        <h2 className="text-xl font-bold">Benvenuto nel Travel Quiz!</h2>
+        <p className="text-md">Metti alla prova le tue conoscenze sui viaggi e scopri quanto ne sai!</p>
+        </div>
+      ) : null}
+
+      {userAnswers.length === TOTAL_QUESTIONS ? (
+        <div className="text-center">
+        <p className="text-black text-2xl m-0">Score: {score}</p>
+        <p className="text-black text-xl m-0 mt-2">Skill Level: {getSkillLevel(score)}</p>
+        </div>
+      ) : null}
+
+
       {!loading && !gameOver &&(
       <QuestionCard
         questionNr={number + 1}
